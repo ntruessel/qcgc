5655ac11a9baf8f8f993c12c6e15bbd8598ca472
Nicolas Truessel <ntruessel@njsm.de>
2016-05-07 14:08:56 +0200

    Fix operator precedence in macros and size of cell

--- arena.h
+++ arena.h
@@ -3,19 +3,21 @@
 #include "config.h"
 #include <stdint.h>
 
-#define QCGC_ARENA_SIZE 1<<QCGC_ARENA_SIZE_EXP
+#define QCGC_ARENA_SIZE (1<<QCGC_ARENA_SIZE_EXP)
 
 #define QCGC_ARENA_ADDR(x) ((void *)(((intptr_t) x) & ~(QCGC_ARENA_SIZE - 1)))
-#define QCGC_ARENA_BITMAP_SIZE 1<<(QCGC_ARENA_SIZE_EXP - 7) // 1 / 128
-#define QCGC_ARENA_CELLS_COUNT (1<<(QCGC_ARENA_SIZE_EXP - 4) - 1<<(QCGC_ARENA_SIZE_EXP - 10))
+#define QCGC_ARENA_BITMAP_SIZE (1<<(QCGC_ARENA_SIZE_EXP - 7)) // 1 / 128
+#define QCGC_ARENA_CELLS_COUNT ((1<<(QCGC_ARENA_SIZE_EXP - 4)) - (1<<(QCGC_ARENA_SIZE_EXP - 10)))
 
 #define QCGC_ARENA_CELL_INDEX(x) ((void *)(((intptr_t)(x)) & (QCGC_ARENA_SIZE - 1)))
+
+typedef uint8_t cell_t[16];
 
 // Currently no need to pack this
 typedef struct arena_s {
 	uint8_t block_bitmap[QCGC_ARENA_BITMAP_SIZE];
 	uint8_t mark_bitmap[QCGC_ARENA_BITMAP_SIZE];
-	uint16_t cells[QCGC_ARENA_CELLS_COUNT];
+	cell_t cells[QCGC_ARENA_CELLS_COUNT];
 } arena_t;
 
 /**
