82281433cdd6168b228438e23dbe1854358b6306
Nicolas <ntruessel@njsm.de>
2016-05-12 18:28:20 +0200

    Fix types of macros

--- arena.h
+++ arena.h
@@ -2,23 +2,26 @@
 
 #include "config.h"
 #include <stdint.h>
+#include <sys/types.h>
+
+typedef struct arena_s arena_t;
 
 #define QCGC_ARENA_SIZE (1<<QCGC_ARENA_SIZE_EXP)
 
-#define QCGC_ARENA_ADDR(x) ((void *)(((intptr_t) x) & ~(QCGC_ARENA_SIZE - 1)))
+#define QCGC_ARENA_ADDR(x) ((arena_t *)(((intptr_t) x) & ~(QCGC_ARENA_SIZE - 1)))
 #define QCGC_ARENA_BITMAP_SIZE (1<<(QCGC_ARENA_SIZE_EXP - 7)) // 1 / 128
 #define QCGC_ARENA_CELLS_COUNT ((1<<(QCGC_ARENA_SIZE_EXP - 4)) - (1<<(QCGC_ARENA_SIZE_EXP - 10)))
 
-#define QCGC_ARENA_CELL_INDEX(x) ((void *)(((intptr_t)(x)) & (QCGC_ARENA_SIZE - 1)))
+#define QCGC_ARENA_CELL_INDEX(x) ((size_t)(((intptr_t)(x)) & (QCGC_ARENA_SIZE - 1)))
 
 typedef uint8_t cell_t[16];
 
 // Currently no need to pack this
-typedef struct arena_s {
+struct arena_s {
 	uint8_t block_bitmap[QCGC_ARENA_BITMAP_SIZE];
 	uint8_t mark_bitmap[QCGC_ARENA_BITMAP_SIZE];
 	cell_t cells[QCGC_ARENA_CELLS_COUNT];
-} arena_t;
+};
 
 /**
  * Create a new arena
