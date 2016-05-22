b0adadb17b399341be7b175a32b8f055091423a8
Nicolas <ntruessel@njsm.de>
2016-05-22 12:34:51 +0200

    Add test for bitmap manipulation and initialization of arena

--- test/build_support.py
+++ test/build_support.py
@@ -12,6 +12,7 @@ ffi.set_source("support",
 
         const size_t qcgc_arena_bitmap_size = QCGC_ARENA_BITMAP_SIZE;
         const size_t qcgc_arena_cells_count = QCGC_ARENA_CELLS_COUNT;
+        const size_t qcgc_arena_first_cell_index = QCGC_ARENA_FIRST_CELL_INDEX;
 
         cell_t *arena_cells(arena_t *arena) {
             return arena->cells;
@@ -34,16 +35,24 @@ ffi.set_source("support",
 ffi.cdef("""
         // config.h
         #define QCGC_SHADOWSTACK_SIZE 4096
-        #define QCGC_ARENA_SIZE_EXP 20		// Between 16 (64kB) and 20 (1MB)
+        #define QCGC_ARENA_SIZE_EXP 20	    // Between 16 (64kB) and 20 (1MB)
 
         // arena.h
         const size_t qcgc_arena_size;
         const size_t qcgc_arena_bitmap_size;
         const size_t qcgc_arena_cells_count;
+        const size_t qcgc_arena_first_cell_index;
 
         typedef uint8_t cell_t[16];
 
         typedef union arena_u arena_t;
+
+        typedef enum blocktype {
+                BLOCK_EXTENT,
+                BLOCK_FREE,
+                BLOCK_WHITE,
+                BLOCK_BLACK,
+        } blocktype_t;
 
         cell_t *arena_cells(arena_t *arena);
         uint8_t *arena_mark_bitmap(arena_t *arena);
@@ -55,6 +64,8 @@ ffi.cdef("""
         size_t qcgc_arena_cell_index(void *);
         bool qcgc_arena_get_bitmap_entry(uint8_t *, size_t);
         void qcgc_arena_set_bitmap_entry(uint8_t *, size_t, bool);
+
+        blocktype_t qcgc_arena_blocktype(void *ptr);
 
         size_t qcgc_arena_sizeof(void);
 
--- test/test_arena.py
+++ test/test_arena.py
@@ -7,9 +7,11 @@ class ArenaTestCase(QCGCTest):
         size = 2**exp
         bitmap = size / 128
         cells = size / 16
+        first_cell_index = 2 * bitmap / 16;
         self.assertEqual(size, lib.qcgc_arena_size)
         self.assertEqual(bitmap, lib.qcgc_arena_bitmap_size)
         self.assertEqual(cells, lib.qcgc_arena_cells_count)
+        self.assertEqual(first_cell_index, lib.qcgc_arena_first_cell_index)
 
     def test_arena_create(self):
         p = lib.qcgc_arena_create()
@@ -19,6 +21,21 @@ class ArenaTestCase(QCGCTest):
                 int(ffi.cast("uint64_t", p))
                     << lib.QCGC_ARENA_SIZE_EXP
                     >> lib.QCGC_ARENA_SIZE_EXP)
+        self.assertEqual(lib.BLOCK_FREE, lib.qcgc_arena_blocktype(ffi.addressof(lib.arena_cells(p)[lib.qcgc_arena_first_cell_index])))
+
+    def test_bitmap_manipulation(self):
+        p = lib.qcgc_arena_create()
+        for i in range(0, 7):
+            import pdb
+            pdb.set_trace()
+            index = i + lib.qcgc_arena_first_cell_index + 8
+            lib.qcgc_arena_set_bitmap_entry(lib.arena_mark_bitmap(p), index, True)
+            for j in range(0, 7):
+                jndex = j + lib.qcgc_arena_first_cell_index + 8
+                if (j <= i):
+                    self.assertEqual(True, lib.qcgc_arena_get_bitmap_entry(lib.arena_mark_bitmap(p), jndex))
+                else:
+                    self.assertEqual(False, lib.qcgc_arena_get_bitmap_entry(lib.arena_mark_bitmap(p), jndex))
 
     def test_arena_size(self):
         self.assertEqual(lib.qcgc_arena_sizeof(), lib.qcgc_arena_size)
@@ -28,7 +45,7 @@ class ArenaTestCase(QCGCTest):
 
         lib.arena_cells(arena)[0][0] = 15
         self.assertEqual(lib.arena_block_bitmap(arena)[0], 15)
-        
+
         lib.arena_cells(arena)[lib.qcgc_arena_bitmap_size // 16][lib.qcgc_arena_bitmap_size % 16] = 3
         self.assertEqual(lib.arena_mark_bitmap(arena)[0], 3)
 
