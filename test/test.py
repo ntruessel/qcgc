dea2eebc98655b7ca1293a0f2a76e74e2573c841
Nicolas <ntruessel@njsm.de>
2016-06-22 08:32:56 +0200

    Test target now return value !=0 if tests fail

--- test/test.py
+++ test/test.py
@@ -1,8 +1,10 @@
 import unittest
+import sys
 from runner.TreeTextTestResult import TreeTextTestResult
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     tests = loader.discover('.')
     runner = unittest.runner.TextTestRunner(verbosity=2, resultclass=TreeTextTestResult)
-    runner.run(tests)
+    result = runner.run(tests)
+    sys.exit(not result.wasSuccessful())
