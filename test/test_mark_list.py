b1b44b9f73f9d32b908d9a47b3fcc28df9745753
Nicolas <ntruessel@njsm.de>
2016-07-02 23:05:37 +0200

    Fix test bugs
    * Not destroying list after test
    * Not assigning on push/drop

--- test/test_mark_list.py
+++ test/test_mark_list.py
@@ -18,7 +18,7 @@ class MarkListTestCase(QCGCTest):
         """Single push and pop"""
         l = lib.qcgc_mark_list_create(1000)
         for i in range(1000):
-            lib.qcgc_mark_list_push(l, ffi.cast("object_t *", i))
+            l = lib.qcgc_mark_list_push(l, ffi.cast("object_t *", i))
 
         i = 0
         while i < 1000:
@@ -28,13 +28,14 @@ class MarkListTestCase(QCGCTest):
                 if i < 1000:
                     self.assertEqual(ffi.cast("object_t *", i), segment[j])
                     i += 1
-            lib.qcgc_mark_list_drop_head_segment(l);
+            l = lib.qcgc_mark_list_drop_head_segment(l)
+        lib.qcgc_mark_list_destroy(l)
 
     def test_grow_push(self):
         """Growing on space exhaustion when using single push"""
         l = lib.qcgc_mark_list_create(200)
         for i in range(1000):
-            lib.qcgc_mark_list_push(l, ffi.cast("object_t *", i))
+            l = lib.qcgc_mark_list_push(l, ffi.cast("object_t *", i))
 
         i = 0
         while i < 1000:
@@ -44,7 +45,8 @@ class MarkListTestCase(QCGCTest):
                 if i < 1000:
                     self.assertEqual(ffi.cast("object_t *", i), segment[j])
                     i += 1
-            lib.qcgc_mark_list_drop_head_segment(l);
+            l = lib.qcgc_mark_list_drop_head_segment(l)
+        lib.qcgc_mark_list_destroy(l)
 
     def test_push_all(self):
         """Push array"""
@@ -58,12 +60,12 @@ class MarkListTestCase(QCGCTest):
             arr[i] = ffi.cast("object_t *", i)
 
         for i in range(pre_fill):
-            lib.qcgc_mark_list_push(l,ffi.NULL)
+            l = lib.qcgc_mark_list_push(l,ffi.NULL)
 
-        lib.qcgc_mark_list_push_all(l, arr, arr_size)
+        l = lib.qcgc_mark_list_push_all(l, arr, arr_size)
 
         for i in range(list_size - arr_size - pre_fill):
-            lib.qcgc_mark_list_push(l,ffi.NULL)
+            l = lib.qcgc_mark_list_push(l,ffi.NULL)
 
         i = 0
         while i < list_size:
@@ -76,6 +78,7 @@ class MarkListTestCase(QCGCTest):
                     self.assertEqual(segment[i], ffi.NULL)
                 i += 1
             segment = lib.qcgc_mark_list_get_head_segment(l)
+        lib.qcgc_mark_list_destroy(l)
 
     def test_grow_push_all(self):
         """Grow on push array"""
@@ -90,9 +93,9 @@ class MarkListTestCase(QCGCTest):
             arr[i] = ffi.cast("object_t *", i)
 
         for i in range(pre_fill):
-            lib.qcgc_mark_list_push(l,ffi.NULL)
+            l = lib.qcgc_mark_list_push(l,ffi.NULL)
 
-        lib.qcgc_mark_list_push_all(l, arr, arr_size)
+        l = lib.qcgc_mark_list_push_all(l, arr, arr_size)
 
         i = 0
         while i < list_size:
@@ -105,3 +108,4 @@ class MarkListTestCase(QCGCTest):
                     self.assertEqual(segment[i], ffi.NULL)
                 i += 1
             segment = lib.qcgc_mark_list_get_head_segment(l)
+        lib.qcgc_mark_list_destroy(l)
