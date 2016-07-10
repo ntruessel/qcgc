c203f53257e4ac873f3361859158024a45b7fb56
Nicolas <ntruessel@njsm.de>
2016-07-10 21:40:53 +0200

    Add additional test for black object barrier

--- test/test_object.py
+++ test/test_object.py
@@ -4,11 +4,20 @@ import unittest
 
 class ObjectTestCase(QCGCTest):
     def test_write_barrier(self):
-        arena = lib.qcgc_arena_create()
         o = self.allocate(16)
+        arena = lib.qcgc_arena_addr(ffi.cast("cell_t *", o))
         self.assertEqual(ffi.cast("object_t *", o).flags & lib.QCGC_GRAY_FLAG, 0)
         lib.qcgc_write(ffi.cast("object_t *", o))
         self.assertEqual(ffi.cast("object_t *", o).flags & lib.QCGC_GRAY_FLAG, lib.QCGC_GRAY_FLAG)
+
+        lib.qcgc_state.state = lib.GC_MARK
+        o = self.allocate(16)
+        self.assertEqual(ffi.cast("object_t *", o).flags & lib.QCGC_GRAY_FLAG, 0)
+        lib.qcgc_arena_set_blocktype(ffi.cast("cell_t *", o), lib.BLOCK_BLACK)
+        lib.qcgc_write(ffi.cast("object_t *", o))
+        self.assertEqual(ffi.cast("object_t *", o).flags & lib.QCGC_GRAY_FLAG, lib.QCGC_GRAY_FLAG)
+        self.assertEqual(lib.arena_gray_stack(arena).index, 1)
+        self.assertEqual(lib.arena_gray_stack(arena).items[0], o)
 
 if __name__ == "__main__":
     unittest.main()
