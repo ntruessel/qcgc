35f2838d1451681f1cc49fba3b4466389bf2cf68
Nicolas <ntruessel@njsm.de>
2016-08-17 17:54:52 +0200

    Add testcase for allocator initialization

--- test/test_allocator.py
+++ test/test_allocator.py
@@ -6,3 +6,12 @@ class AllocatorTest(QCGCTest):
         for i in range(1,17):
             self.assertEqual(1, lib.bytes_to_cells(i))
         self.assertEqual(2, lib.bytes_to_cells(17))
+
+    def test_init_values(self):
+        self.assertNotEqual(ffi.NULL, lib.arenas)
+        for i in range(lib.qcgc_small_free_lists):
+            l = lib.small_free_list(i)
+            self.assertNotEqual(ffi.NULL, l)
+        for i in range(lib.qcgc_large_free_lists):
+            l = lib.large_free_list(i)
+            self.assertNotEqual(ffi.NULL, l)
