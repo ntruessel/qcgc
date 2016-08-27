f6d0240cfce616cf359a6544c0a6aa13795c8ee2
Nicolas <ntruessel@njsm.de>
2016-08-27 10:16:21 +0200

    Write test for incremental large block marking

--- test/test_large_allocate.py
+++ test/test_large_allocate.py
@@ -71,6 +71,38 @@ class LargeAllocateTestCase(QCGCTest):
         self.assertEqual(lib.qcgc_arena_get_blocktype(
             ffi.cast("cell_t *", r)), lib.BLOCK_WHITE)
 
+    def test_incremenatal(self):
+        o = ffi.cast("object_t *", self.allocate_ref(lib.qcgc_arena_size // ffi.sizeof("myobject_t *")))
+        p = ffi.cast("object_t *", self.allocate(1))
+        q = ffi.cast("object_t *", self.allocate(1))
+        self.set_ref(o, 0, p)
+        #
+        lib.qcgc_shadowstack_push(o)
+        lib.qcgc_mark_incremental()
+        #
+        self.assertTrue(self.hbtable_has(o))
+        self.assertTrue(self.hbtable_marked(o))
+        self.assertFalse(self.hbtable_gray_stack_has(o))
+        #
+        self.assertEqual(lib.qcgc_arena_get_blocktype(
+            ffi.cast("cell_t *", p)), lib.BLOCK_BLACK)
+        self.assertEqual(lib.qcgc_arena_get_blocktype(
+            ffi.cast("cell_t *", q)), lib.BLOCK_WHITE)
+        #
+        self.set_ref(o, 1, q)
+        self.assertTrue(self.hbtable_gray_stack_has(o))
+        #
+        lib.qcgc_mark_incremental()
+        #
+        self.assertTrue(self.hbtable_has(o))
+        self.assertTrue(self.hbtable_marked(o))
+        self.assertFalse(self.hbtable_gray_stack_has(o))
+        #
+        self.assertEqual(lib.qcgc_arena_get_blocktype(
+            ffi.cast("cell_t *", p)), lib.BLOCK_BLACK)
+        self.assertEqual(lib.qcgc_arena_get_blocktype(
+            ffi.cast("cell_t *", q)), lib.BLOCK_BLACK)
+
     def hbtable_has(self, o):
         b = lib.bucket(o)
         for i in range(lib.qcgc_hbtable.bucket[b].count):
