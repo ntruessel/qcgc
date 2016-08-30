c24a640c6beff9a8600db3baa6606f9784400584
Nicolas Truessel <ntruessel@njsm.de>
2016-08-30 22:01:01 +0200

    Update gdb arena commands

--- tools/gdb/arena.py
+++ tools/gdb/arena.py
@@ -16,7 +16,7 @@ class ArenaCoalesceError(gdb.Command):
         first_cell = cells // 64
         last_free = None
         for i in range(first_cell, cells):
-            bt = self._blocktype(arena, i)
+            bt = blocktype(arena, i)
             if (bt == Blocktype.free):
                 if last_free is None:
                     last_free = i
@@ -27,22 +27,42 @@ class ArenaCoalesceError(gdb.Command):
                 last_free = None
 
 
-    def _blocktype(self, arena, index):
-        mark_bit = self._bitmap_entry(arena['mark_bitmap'], index)
-        block_bit = self._bitmap_entry(arena['block_bitmap'], index)
-        if block_bit:
-            if mark_bit:
-                return Blocktype.black
-            else:
-                return Blocktype.white
-        else:
-            if mark_bit:
-                return Blocktype.free
-            else:
-                return Blocktype.extent
 
-    def _bitmap_entry(self, bitmap, index):
-        return (((bitmap[index // 8] >> (index % 8)) & 0x1) == 0x1);
+class BlocktypeLookup(gdb.Command):
+    def __init__(self):
+        super(BlocktypeLookup, self).__init__("blocktype-lookup", gdb.COMMAND_USER, gdb.COMPLETE_EXPRESSION)
+
+    def invoke(self, arg, from_tty):
+        ptr = gdb.parse_and_eval(arg)
+        arena = ptr_to_arena(ptr)
+        print(blocktype(arena, index(ptr)))
+
+def ptr_to_arena(ptr):
+    return gdb.Value(int(ptr) & ~0x0fffff).cast(gdb.lookup_type("arena_t").pointer()).dereference() #XXX: Lookup arena size
+
+def index(ptr):
+    arena = ptr_to_arena(ptr)
+    cell_ptr_t = gdb.lookup_type("cell_t").pointer()
+    result = ptr.cast(cell_ptr_t) - arena.address.cast(cell_ptr_t)
+    assert arena['cells'][result].address == ptr
+    return int(result)
+
+def blocktype(arena, index):
+    mark_bit = bitmap_entry(arena['mark_bitmap'], index)
+    block_bit = bitmap_entry(arena['block_bitmap'], index)
+    if block_bit:
+        if mark_bit:
+            return Blocktype.black
+        else:
+            return Blocktype.white
+    else:
+        if mark_bit:
+            return Blocktype.free
+        else:
+            return Blocktype.extent
+
+def bitmap_entry(bitmap, index):
+    return (((bitmap[index // 8] >> (index % 8)) & 0x1) == 0x1);
 
 
 class Blocktype(Enum):
@@ -55,3 +75,4 @@ class Blocktype(Enum):
         return (self.value)
 
 ArenaCoalesceError()
+BlocktypeLookup()
