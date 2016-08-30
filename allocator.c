f90221454db8b6d22fc74184c934d9022eb1d9eb
Nicolas Truessel <ntruessel@njsm.de>
2016-08-30 10:55:48 +0200

    Add invariant to trace origin of stresstest bug

--- allocator.c
+++ allocator.c
@@ -119,6 +119,17 @@ object_t *qcgc_bump_allocate(size_t bytes) {
 	result->flags |= QCGC_GRAY_FLAG;
 #if CHECKED
 	assert(qcgc_arena_is_coalesced(qcgc_arena_addr((cell_t *)result)));
+	if (qcgc_arena_addr(qcgc_allocator_state.bump_state.bump_ptr) !=
+			(arena_t *) qcgc_allocator_state.bump_state.bump_ptr) {
+		assert(qcgc_arena_get_blocktype(
+					qcgc_allocator_state.bump_state.bump_ptr) == BLOCK_FREE);
+		for (size_t i = 1; i < qcgc_allocator_state.bump_state.remaining_cells;
+				i++) {
+			assert(qcgc_arena_get_blocktype(
+						qcgc_allocator_state.bump_state.bump_ptr + i)
+					== BLOCK_EXTENT);
+		}
+	}
 #endif
 	return result;
 }
